8f39eb1f756ba40448fb946b5f3303098a88fc75
Tommi <tommi.korteikko@iCloud.com>
2024-01-06 22:34:32 +0200

    inital commit

---
+++ src/main/java/com/myjava/expencetracker/expencetracker/expencetracker/ExpencetrackerApplication.java
@@ -0,0 +1,13 @@
+package com.myjava.expencetracker.expencetracker.expencetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpencetrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpencetrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myjava/expencetracker/expencetracker/expencetracker/ExpencetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myjava.expencetracker.expencetracker.expencetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpencetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
